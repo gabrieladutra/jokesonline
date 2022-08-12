891b86f00b28f8d289107232b42866309a56a364
gabrieladutra <gabriela_ddutra@outlook.com>
2022-08-12 19:49:33 -0300

    Added the method to display the joke

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function getRandomJoke(max) {
     return Math.floor(Math.random() * max)
 }
+let jokes = ["Qual é a cidade brasileira que não tem táxi? R: Uberlândia.", "Por que o rádio"];
+jokes.push("test")
+
+function print1() {
+    //document.getElementById("test").innerHTML = jokes[getRandomJoke(jokes.length)] }
+    document.getElementById("msg").innerHTML = jokes[getRandomJoke(jokes.length)]
+}
